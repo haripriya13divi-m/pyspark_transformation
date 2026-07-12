0065e9bb26d9ea7c4adfae945817b1fb31a9b9c3
haripriya13divi-m <haripriya13divi@gmail.com>
2026-07-12 20:37:57 +0530

    Add data cleaning steps for missing values

--- Trans_clean.py
+++ Trans_clean.py
@@ -4,6 +4,7 @@ from pyspark.sql.functions import  col, lit
 from pyspark.sql.types import *
 
 df = spark.read.csv("/Workspace/Users/haripriya13divi@gmail.com/Employee.csv",header = True, inferSchema = True)
+#claendata
 df_clean = df.fillna("India",["city"]) \
              .fillna(10000,["salary"]) \
              .fillna(0,["experience"])
